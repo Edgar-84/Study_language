63f838bd5bab6f8c2eb99a67e5a368603ca1f62a
edgar84 <edgar.tomko.97@mail.ru>
2022-07-18 17:29:27 +0300

    change models, add get_absolute_url

--- cards/models.py
+++ cards/models.py
@@ -1,13 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 class Category(models.Model):
     user = models.ForeignKey(User, related_name="category_created", on_delete=models.CASCADE)
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=100, db_index=True)
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('category', kwargs={'cat_id': self.pk})
 
     class Meta:
         verbose_name = "Category"
