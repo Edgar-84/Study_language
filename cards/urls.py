ac9d85fcab3716490080f590673d4d31abd071b4
edgar84 <edgar.tomko.97@mail.ru>
2022-07-20 17:01:35 +0300

    create new views and urls, add slug

--- cards/urls.py
+++ cards/urls.py
@@ -3,6 +3,11 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', index, name='cards'),
+    path('', index, name='home'),
     path('about/', about, name='about'),
+    path('card_info/<slug:card_slug>', show_card, name='card'),
+    path('start_lesson/', start_lesson, name='start_lesson'),
+    path('contact/', contact, name='contact'),
+    path('login/', login, name='login'),
+    path('category/<int:cat_id>/', show_category, name='category'),
 ]
--- cards/views.py
+++ cards/views.py
@@ -1,23 +1,66 @@
 from django.http import HttpResponse, HttpResponseNotFound
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import *
 
 
-menu = ["О сайте", "Мои карточки", "Начать занятие", "Обратная связь", "Войти"]
+menu = [{"title": "Главная", "url_name": "home"},
+        {"title": "О сайте", "url_name": "about"},
+        {"title": "Начать занятие", "url_name": "start_lesson"},
+        {"title": "Обратная связь", "url_name": "contact"},
+        {"title": "Войти", "url_name": "login"}
+]
 
 
 def index(request):
-    posts = Category.objects.all()
-    return render(request, "cards/index.html", {'posts': posts, 'title': 'Главная страница', 'menu': menu})
+    cats = Category.objects.filter(user=request.user)
+
+    context = {
+        'cats': cats,
+        'title': 'Главная страница',
+        'menu': menu
+    }
+    return render(request, "cards/index.html", context=context)
+
+
+def show_category(request, cat_id):
+    words = Card.objects.filter(category=cat_id)
+    cats = Category.objects.filter(user=request.user)
+
+    context = {
+        'cats': cats,
+        'cards': words,
+        'title': 'Отобажение слов из выбранного списка',
+        'menu': menu
+    }
+    return render(request, "cards/show_cards.html", context=context)
+
+
+def show_card(request, card_slug):
+    card = get_object_or_404(Card, slug=card_slug)
+
+    context = {
+        'card': card,
+        'menu': menu,
+        'title': card.title_native_language,
+    }
+    return render(request, 'cards/card_info.html', context=context)
 
 
 def about(request):
     return render(request, "cards/about.html", {'title': 'О сайте', 'menu': menu})
 
 
-def cards(request, card):
-    return HttpResponse(f"<h1>Cards number</h1><p>{card}</p>")
+def start_lesson(request):
+    return HttpResponse('Запуск уроков')
+
+
+def contact(request):
+    return HttpResponse('Обратная связь')
+
+
+def login(request):
+    return HttpResponse('Авторизация')
 
 
 def pageNotFound(request, exception):
